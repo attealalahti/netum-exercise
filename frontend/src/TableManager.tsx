852cc9a6098787728280bf59768d3e3c920cd233
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-02-15 17:15:27 +0200

    Add dropdown button for sorting. Modify style to fit.

--- frontend/src/TableManager.tsx
+++ frontend/src/TableManager.tsx
@@ -1,4 +1,4 @@
-import { Table, Button } from "react-bootstrap";
+import { Table, Button, DropdownButton, Dropdown } from "react-bootstrap";
 
 interface Person {
     firstName: string;
@@ -13,33 +13,42 @@ let db: Person[] = [
 
 function TableManager() {
     return (
-        <div>
-            <div className="TableContainer">
-                <Table striped bordered>
-                    <thead>
+        <div className="TableContainer">
+            <DropdownButton
+                title="Sort by"
+                variant="secondary"
+                className="DropdownButton"
+            >
+                <Dropdown.Item>First Name</Dropdown.Item>
+                <Dropdown.Item>Last Name</Dropdown.Item>
+                <Dropdown.Item>Age</Dropdown.Item>
+            </DropdownButton>
+            <Table striped bordered>
+                <thead>
+                    <tr>
                         <th></th>
                         <th>First Name</th>
                         <th>Last Name</th>
                         <th>Age</th>
                         <th></th>
-                    </thead>
-                    <tbody>
-                        {db.map((person) => (
-                            <tr>
-                                <td className="ButtonCell">
-                                    <Button variant="primary">Edit</Button>
-                                </td>
-                                <td>{person.firstName}</td>
-                                <td>{person.lastName}</td>
-                                <td>{person.age}</td>
-                                <td className="ButtonCell">
-                                    <Button variant="danger">Delete</Button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </Table>
-            </div>
+                    </tr>
+                </thead>
+                <tbody>
+                    {db.map((person, index) => (
+                        <tr key={index}>
+                            <td className="ButtonCell">
+                                <Button variant="primary">Edit</Button>
+                            </td>
+                            <td>{person.firstName}</td>
+                            <td>{person.lastName}</td>
+                            <td>{person.age}</td>
+                            <td className="ButtonCell">
+                                <Button variant="danger">Delete</Button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
         </div>
     );
 }
