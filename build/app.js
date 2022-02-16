4d5e3f7394fe1225987fa3ec6d7621151382dc65
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-02-16 15:15:20 +0200

    Modify app.ts to add express serving.

--- build/app.js
+++ build/app.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("hello world");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => {
+    console.log(`Listening on port ${server.address().port}`);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,6 @@
-console.log("hello world");
+import express from "express";
+const app = express();
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => {
+    console.log(`Listening on port ${(server.address() as any).port}`);
+});
