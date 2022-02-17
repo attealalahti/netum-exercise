fc99db04481f81bc7b74b2abf669250a2aea5cfd
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-02-17 20:24:20 +0200

    Modify app.ts localhost port to 3000.

--- build/app.js
+++ build/app.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const people_1 = __importDefault(require("./routes/people"));
 const app = (0, express_1.default)();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
     console.log(`Listening on port ${server.address().port}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import people from "./routes/people";
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
     console.log(`Listening on port ${(server.address() as any).port}`);
 });
