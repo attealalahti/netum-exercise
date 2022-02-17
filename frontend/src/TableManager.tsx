ae4864cd79e91fbde8b3a15401035ce180f2faa5
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-02-17 20:00:18 +0200

    Modify TableManager component to fetch and use data from the database.

--- frontend/src/TableManager.tsx
+++ frontend/src/TableManager.tsx
@@ -1,58 +1,81 @@
-interface Person {
-    firstName: string;
-    lastName: string;
-    age: number;
-}
+import { FC, useState, useEffect } from "react";
+import axios from "axios";
 
-let db: Person[] = [
-    { firstName: "John", lastName: "Watson", age: 45 },
-    { firstName: "Sherlock", lastName: "Holmes", age: 33 },
-];
+const TableManager: FC = () => {
+    interface Person {
+        id: number;
+        first_name: string;
+        last_name: string;
+        age: number;
+    }
+    const [people, setPeople] = useState<Person[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<boolean>(false);
 
-function TableManager() {
-    return (
-        <div className="TableContainer">
-            <div className="Dropdown">
-                <select id="sort">
-                    <option>Sort by:</option>
-                    <option>First Name</option>
-                    <option>Last Name</option>
-                    <option>Age</option>
-                </select>
-            </div>
-            <table>
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Age</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {db.map((person, index) => (
-                        <tr key={index}>
-                            <td className="ButtonCell">
-                                <button>Edit</button>
-                            </td>
-                            <td>
-                                <span className="CellText">{person.firstName}</span>
-                            </td>
-                            <td>
-                                <span className="CellText">{person.lastName}</span>
-                            </td>
-                            <td>
-                                <span className="CellText">{person.age}</span>
-                            </td>
-                            <td className="ButtonCell">
-                                <button>Delete</button>
-                            </td>
+    useEffect(() => {
+        fetchData();
+    });
+
+    const fetchData = async () => {
+        try {
+            const response = (await axios.get("/people")) as Person[];
+            setPeople(response);
+        } catch (err) {
+            setError(true);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    if (loading) {
+        return <p>Loading...</p>;
+    } else if (error) {
+        return <p>An error occurred.</p>;
+    } else {
+        return (
+            <div className="TableContainer">
+                <div className="Dropdown">
+                    <select id="sort">
+                        <option>Sort by:</option>
+                        <option>First Name</option>
+                        <option>Last Name</option>
+                        <option>Age</option>
+                    </select>
+                </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Age</th>
+                            <th></th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
-}
+                    </thead>
+                    <tbody>
+                        {people.map((person) => (
+                            <tr key={person.id}>
+                                <td className="ButtonCell">
+                                    <button>Edit</button>
+                                </td>
+                                <td>
+                                    <span className="CellText">{person.first_name}</span>
+                                </td>
+                                <td>
+                                    <span className="CellText">{person.last_name}</span>
+                                </td>
+                                <td>
+                                    <span className="CellText">{person.age}</span>
+                                </td>
+                                <td className="ButtonCell">
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+};
 export default TableManager;
