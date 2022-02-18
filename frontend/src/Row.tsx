d11ebe87623495fb88e74202d5acca2440bc103a
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-02-18 17:51:58 +0200

    Modify Row to add editing mode and functions to activate it.

--- frontend/src/Row.tsx
+++ frontend/src/Row.tsx
@@ -16,29 +16,71 @@ const Row: FC<Props> = (props) => {
         }
     };
 
-    return (
-        <tr>
-            <td className="ButtonCell">
-                <button className={props.editing && !editingThis ? "Gray" : "Blue"}>
-                    Edit
-                </button>
-            </td>
-            <td>
-                <span className="CellText">{props.person.first_name}</span>
-            </td>
-            <td>
-                <span className="CellText">{props.person.last_name}</span>
-            </td>
-            <td>
-                <span className="CellText">{props.person.age}</span>
-            </td>
-            <td className="ButtonCell">
-                <button className={props.editing ? "Gray" : "Red"} onClick={handleDelete}>
-                    Delete
-                </button>
-            </td>
-        </tr>
-    );
+    const startEditing = () => {
+        if (!props.editing) {
+            props.setEditing(true);
+            setEditingThis(true);
+        }
+    };
+    const stopEditing = () => {
+        props.setEditing(false);
+        setEditingThis(false);
+    };
+
+    if (editingThis) {
+        return (
+            <tr>
+                <td className="ButtonCell">
+                    <button className="Green">Save</button>
+                </td>
+                <td>
+                    <input className="EditInput" defaultValue={props.person.first_name} />
+                </td>
+                <td>
+                    <input className="EditInput" defaultValue={props.person.last_name} />
+                </td>
+                <td className="AgeCell">
+                    <input className="EditInput" defaultValue={props.person.age} />
+                </td>
+                <td className="ButtonCell">
+                    <button className="Blue" onClick={stopEditing}>
+                        Cancel
+                    </button>
+                </td>
+            </tr>
+        );
+    } else {
+        return (
+            <tr>
+                <form />
+                <td className="ButtonCell">
+                    <button
+                        className={props.editing && !editingThis ? "Gray" : "Blue"}
+                        onClick={startEditing}
+                    >
+                        Edit
+                    </button>
+                </td>
+                <td>
+                    <span className="CellText">{props.person.first_name}</span>
+                </td>
+                <td>
+                    <span className="CellText">{props.person.last_name}</span>
+                </td>
+                <td className="AgeCell">
+                    <span className="CellText">{props.person.age}</span>
+                </td>
+                <td className="ButtonCell">
+                    <button
+                        className={props.editing ? "Gray" : "Red"}
+                        onClick={handleDelete}
+                    >
+                        Delete
+                    </button>
+                </td>
+            </tr>
+        );
+    }
 };
 
 export default Row;
